4e87a4eca055ba4292efd8fa2dff26315aebb5ae
gorann1 <gorandevop@outlook.com>
2023-04-21 11:41:06 +0200

    #sign_in_sign_out_session_header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,4 @@
+import { signIn, signOut,useSession } from 'next-auth/react'
 import * as React from 'react';
 
 import UnstyledLink from '@/components/links/UnstyledLink';
@@ -8,12 +9,26 @@ const links = [
 ];
 
 export default function Header() {
+  const { data: session } = useSession();
+
+  const handleSignin = (e) => {
+    e.preventDefault()
+    signIn()
+  }
+
+  const handleSignout = (e) => {
+    e.preventDefault()
+    signOut()
+  }
+
   return (
     <header className='sticky top-0 z-50 bg-white'>
       <div className='layout flex h-14 items-center justify-between'>
         <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
           Home
         </UnstyledLink>
+        {session && <a href="#" onClick={handleSignout} className="btn-signin">Sign out</a>  }
+        {!session && <a href="#" onClick={handleSignin}  className="btn-signin">Sign in</a>  }
         <nav>
           <ul className='flex items-center justify-between space-x-4'>
             {links.map(({ href, label }) => (
